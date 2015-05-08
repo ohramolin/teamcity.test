1a9b0ccb623105bd47e00fd6ba2a74c1ee2983bb
ohramolin <oh@interlogic.com.ua>
2015-05-08 10:23:55 +0200

    initial commit

---
+++ teamcity.test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="teamcity.test.MvcApplication" Language="C#" %>
---
+++ teamcity.test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(teamcity.test.Startup))]
+namespace teamcity.test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
